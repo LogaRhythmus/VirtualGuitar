e93251b4db3d72a129c6c58d41262697611ea647
MarvelMartin <Martin@maerz-von-steinbach.de>
2015-05-21 19:59:04 +0200

    default values

--- AirGuitar/MyoHelper.cs
+++ AirGuitar/MyoHelper.cs
@@ -14,7 +14,7 @@ namespace AirGuitar
         {
             return null;
         }
-        // default values: <TOdo>
+        // default values: 25, 60, 30, 4, 3
         public void init(int radius_cm, int angle_x, int angle_y, int frets_x, int frets_y)
         {
             
